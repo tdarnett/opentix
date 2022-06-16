a688f41e59fca1b24cbdaa3d97e392d9da6635da
Taylor Arnett <tarnett1@mac.com>
2022-06-15 21:07:33 -0600

    custom token status mapping and test

--- contracts/contracts/nft.py
+++ contracts/contracts/nft.py
@@ -1,23 +1,49 @@
+from enum import Enum
+
 import smartpy as sp
 
 FA2 = sp.io.import_script_from_url("https://smartpy.io/templates/fa2_lib.py")
 
 
+class NFTStatus(Enum):
+    MINTED = 1
+    CLAIMED = 2
+    VOID = 3
+
+
 class NFT(
     FA2.Admin,
-    # FA2.ChangeMetadata,
     FA2.OffchainviewTokenMetadata,
     FA2.MintNft,
-    FA2.Fa2Nft
+    FA2.Fa2Nft,
 ):
-
     def __init__(self, administrator, **kwargs):
         FA2.Fa2Nft.__init__(self, **kwargs)
         FA2.Admin.__init__(self, administrator)
+        self.update_initial_storage(
+            # The big map with the token's status
+            token_status=sp.big_map({}, tkey=sp.TNat, tvalue=sp.TNat),
+        )
 
-    @sp.offchain_view()
+    @sp.entry_point
+    def mint(self, batch):
+        """Admin can mint new or existing tokens."""
+        sp.verify(self.is_administrator(sp.sender), "FA2_NOT_ADMIN")
+        with sp.for_("action", batch) as action:
+            token_id = sp.compute(self.data.last_token_id)
+            metadata = sp.record(token_id=token_id, token_info=action.metadata)
+            self.data.token_metadata[token_id] = metadata
+            self.data.ledger[token_id] = action.to_
+            self.data.token_status[token_id] = NFTStatus.MINTED.value
+            self.data.last_token_id += 1
+
+    @sp.onchain_view(pure=True)
     def is_claimed(self, token_id):
-        """Return the claimed state of a given token"""
-        sp.trace(sp.unpack(self.data.token_metadata[token_id].token_info['claimed'], sp.TString))
-        sp.result(self.data.token_metadata[token_id].token_info['claimed'])
+        """If the token has been claimed yet"""
+        sp.set_type(token_id, sp.TNat)
+        self._check_token_exists(token_id)
+        sp.result(self.data.token_status[token_id] == NFTStatus.CLAIMED.value)
 
+    def _check_token_exists(self, token_id):
+        """Checks that the given token exists."""
+        sp.verify(token_id < self.data.last_token_id, message="FA2_TOKEN_UNDEFINED")
--- contracts/tests/nft_test.py
+++ contracts/tests/nft_test.py
@@ -2,20 +2,16 @@ import smartpy as sp
 
 nft_module = sp.io.import_script_from_url("file:contracts/nft.py")
 
+
 def make_metadata(symbol, name):
     """Helper function to build metadata JSON bytes values."""
     return sp.map(
         l={
             "name": sp.utils.bytes_of_string(name),
             "symbol": sp.utils.bytes_of_string(symbol),
-            "claimed": sp.utils.bytes_of_string("false") # map doenst support anything by TBytes
         }
     )
 
-
-# admin = sp.test_account("Administrator")
-# alice = sp.test_account("Alice")
-tok0_md = make_metadata(name="Cowboys SZN22", symbol="CBY22")
 
 def get_test_environment():
     # Initialize the test scenario
@@ -31,13 +27,6 @@ def get_test_environment():
     fa2 = nft_module.NFT(
         administrator=admin.address,
         metadata=sp.utils.metadata_of_url("ipfs://aaa"),
-        token_metadata=[tok0_md],
-        policy=None,
-        ledger={
-            0: user1.address,
-            # 1: user2.address,
-            # 2: user3.address,
-        },
     )
     scenario += fa2
 
@@ -48,32 +37,32 @@ def get_test_environment():
         "user1": user1,
         "user2": user2,
         "user3": user3,
-        "fa2": fa2
+        "fa2": fa2,
     }
 
 
 @sp.add_test(name="Test Mint")
 def test_mint_nft():
     test_env = get_test_environment()
-    scenario = test_env['scenario']
+    scenario = test_env["scenario"]
     admin = test_env["admin"]
     user1 = test_env["user1"]
     fa2 = test_env["fa2"]
 
-    # Attempt to mint  a token from a non-admin account and get an error
+    tok0_md = make_metadata(name="Cowboys SZN22", symbol="CBY22")
+
+    scenario.h3("Mint from non-admin account")
+    # Attempt to mint a token from a non-admin account and get an error
     fa2.mint([sp.record(metadata=tok0_md, to_=user1.address)]).run(
         sender=user1, valid=False, exception="FA2_NOT_ADMIN"
     )
 
-    scenario.h3("Mint from non-admin account")
+    scenario.h3("Mint from admin account")
     # Mint of a new NFT token from a non-admin account
-    fa2.mint(
-        [
-            sp.record(metadata=tok0_md, to_=user1.address),
-        ]
-    ).run(sender=admin)
-    # THEN alice's balance should increase
-    # TODO add tests to check the metadata for said token
+    fa2.mint([sp.record(metadata=tok0_md, to_=user1.address)]).run(sender=admin)
+
     scenario.verify(fa2.get_balance(sp.record(owner=user1.address, token_id=0)) == 1)
-    scenario.show(fa2.is_claimed(0))
-    scenario.show(fa2.token_metadata(0).token_info['claimed'])
+    scenario.verify(sp.len(fa2.all_tokens()) == 1)
+    scenario.verify(fa2.token_metadata(0).token_info["name"] == tok0_md["name"])
+    scenario.verify(fa2.token_metadata(0).token_info["symbol"] == tok0_md["symbol"])
+    scenario.verify(~fa2.is_claimed(0))
